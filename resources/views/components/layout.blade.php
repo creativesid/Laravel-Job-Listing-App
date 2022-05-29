62f6a9c6208c0a734cf02ed6a2086d94e778777b
siddharth pandey <siddharthpandey091@gmail.com>
2022-05-29 14:39:03 +0530

    initial commit

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -26,7 +26,7 @@
 </head>
 
 <body class="mb-48">
-    <nav class="flex justify-between items-center mb-4">
+    <nav class="flex justify-between items-center mb-4 md:mx-10 mx-0">
         <a href="/"><img class="w-24" src={{ asset('images/logo.png') }} alt="" class="logo" /></a>
         <ul class="flex space-x-6 mr-6 text-lg">
             @auth
@@ -56,7 +56,7 @@
             @endauth
         </ul>
     </nav>
-    <main>
+    <main class="md:mx-20 mx-0">
         {{ $slot }}
     </main>
     <footer
